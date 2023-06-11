94edd66e7dd61d6d16eccfa63da6e2b1a068c187
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-06-11 13:20:47 -0400

    final

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const h3 = document.createElement("h3");
 h3.textContent = "Click on the cat, to see what it has to say!";
 document.querySelector("body").appendChild(h3);
 
+
+const imageTag = document.getElementById("clickImage")
+imageTag.addEventListener("click", function(){
+    alert("give me treats!")
+});
+
+
