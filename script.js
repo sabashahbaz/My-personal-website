b456182ea6028c0e6b01c7c0f0e8e42c42993c4d
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-06-11 11:47:55 -0400

    image and text

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This cat has a very important message to deliver";
 document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "Click on the cat, to see what it has to say!";
+document.querySelector("body").appendChild(h3);
